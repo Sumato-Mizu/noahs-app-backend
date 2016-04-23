ae9fcfbf6f3b1c129d4ac5065bf9c79ebffb2318
jkarenko <jk@noo.se>
2016-04-23 17:27:10 +0300

    modify image values of image converter script for better intensity representation.

--- scripts/convertImage.sh
+++ scripts/convertImage.sh
@@ -2,9 +2,9 @@
 input="$1"
 output="$1-new.png"
 convert $input -fill white -opaque black -colors 255 -transparent white $output
-colors=("blue" "lime" "yellow" "orange" "red" "pink")
+colors=("cyan" "blue" "lime" "yellow" "orange" "red")
 index=0
-for i in `seq 0 50 250`; do
-  convert $output -fuzz 30% -fill ${colors[$index]} -opaque "rgb(${i},${i},${i})" $output;
+for i in `seq 0 28 160`; do
+  convert $output -fuzz 15% -fill ${colors[$index]} -opaque "rgb(${i},${i},${i})" $output;
   ((index++));
 done
